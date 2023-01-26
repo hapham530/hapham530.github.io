597ac75968456dacaf27665ce3cefc6c3111202b
hapham <pham.ngoc.ha530@gmail.com>
2023-01-26 23:53:46 +0900

    update

---
+++ build/precache-manifest.fb5f64a5828b242b895affc2dfc88d51.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3bc79738b499da9c2eb8e3b6098c451a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3b6b158ba4a42618e309",
+    "url": "/static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "4a257574504713e82d2c",
+    "url": "/static/css/main.01555ba0.chunk.css"
+  },
+  {
+    "revision": "3b6b158ba4a42618e309",
+    "url": "/static/js/2.a08dfd1c.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/static/js/2.a08dfd1c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4a257574504713e82d2c",
+    "url": "/static/js/main.86c8c56e.chunk.js"
+  },
+  {
+    "revision": "a2d41c56873c741b51b2",
+    "url": "/static/js/runtime-main.a5042f6b.js"
+  }
+]);
